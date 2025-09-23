ab6cfe59befbc92ab920d820ba6316de28ddfb27
Siamoo <walidsyam.f@gmail.com>
2025-09-23 21:22:49 +0300

    Add TextApp & CustomLinearButton

---
+++ lib/core/common/widgets/custom_linear_button.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:znoona_game_app/core/helpers/znoona.colors.dart';
+
+class CustomLinearButton extends StatelessWidget {
+  const CustomLinearButton({
+    required this.onPressed,
+    required this.child,
+    this.height,
+    this.width,
+    super.key,
+  });
+  final VoidCallback onPressed;
+  final Widget child;
+  final double? height;
+  final double? width;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: ZnoonaColors.bluePinkLight(context).withValues(alpha: 0.3),
+      onTap: onPressed,
+      child: Container(
+        height: height ?? 44.h,
+        width: width ?? 44.w,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          gradient: LinearGradient(
+            colors: [
+              ZnoonaColors.bluePinkLight(context),
+              ZnoonaColors.bluePinkDark(  context),
+            ],
+            begin: const Alignment(0.46, -0.89),
+            end: const Alignment(-0.46, 0.89),
+          ),
+        ),
+        child: Center(child: child),
+      ),
+    );
+  }
+}

9e9d4726415af539ba632e91a17cfd8c1d4d4886
Siamoo <walidsyam.f@gmail.com>
2025-09-25 14:09:22 +0300

    Add  packages -> shared_preferences & freezed & build_runner

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:znoona_game_app/core/app/bloc_observer.dart';
 import 'package:znoona_game_app/core/app/env.variables.dart';
 import 'package:znoona_game_app/my_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EnvVariables.instance.init(envType: EnvTypeEnum.prod);
+  Bloc.observer = AppBlocObserver();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,

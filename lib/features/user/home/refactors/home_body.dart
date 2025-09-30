6289cc1091bdab3602353d84946deed0b22af745
Siamoo <walidsyam.f@gmail.com>
2025-09-30 23:40:48 +0300

    Create Home and recive profile in home_body

--- lib/features/user/home/refactors/home_body.dart
+++ lib/features/user/home/refactors/home_body.dart
@@ -1,38 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:znoona_game_app/core/helpers/znoona_navigate.dart';
+import 'package:znoona_game_app/features/user/auth/domain/entities/profile.dart';
 import 'package:znoona_game_app/features/user/auth/presentation/screens/login_screen.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:znoona_game_app/features/user/auth/presentation/cubit/auth_cubit.dart';
 
 class HomeBody extends StatelessWidget {
-  const HomeBody({super.key});
-
+  const HomeBody({required this.profile, super.key, });
+ final Profile profile;
   @override
   Widget build(BuildContext context) {
-    // final profile = context.select<AuthCubit, dynamic>(
-    //   (cubit) => cubit.state.maybeWhen(
-    //     authenticated: (profile) => profile,
-    //     orElse: () => null,
-    //   ),
-    // );
-
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () async {
-                await Supabase.instance.client.auth.signOut();
-                ZnoonaNavigate.pushReplacementTo(context, const LoginScreen());
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 60),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(profile.avatarUrl!),
+                    backgroundColor: Colors.transparent,
+                  ),
+                ],
               ),
-              child: const Text('Logout'),
-            ),
-          ],
+              const Spacer(),
+
+              ElevatedButton(
+                onPressed: () async {
+                  await Supabase.instance.client.auth.signOut();
+                  ZnoonaNavigate.pushReplacementTo(
+                    context,
+                    const LoginScreen(),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 60),
+                ),
+                child: const Text('Logout'),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/features/user/home/screens/home_screen.dart
+++ lib/features/user/home/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:znoona_game_app/features/user/auth/presentation/cubit/auth_cubit.dart';
 import 'package:znoona_game_app/features/user/home/refactors/home_body.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,6 +8,16 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const HomeBody();
+    return BlocBuilder<AuthCubit, AuthState>(
+      builder: (context, state) {
+        return state.maybeWhen(
+          authenticated: (profile) => HomeBody(profile: profile),
+          googleauthenticated: (profile) => HomeBody(profile: profile),
+          orElse: () {
+            return Container();
+          },
+        );
+      },
+    );
   }
 }

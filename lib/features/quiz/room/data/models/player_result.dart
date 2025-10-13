323495c2fd575faf3bf159bd67b61e5bda8421c8
Siamoo <walidsyam.f@gmail.com>
2025-10-14 00:47:17 +0300

    Create PlayerResult in Models

---
+++ lib/features/quiz/room/data/models/player_result.dart
@@ -0,0 +1,28 @@
+class PlayerResult {
+  final String userId;
+  final String username;
+  final int score;
+  final int correctAnswers;
+  final int totalQuestions;
+  final int rank;
+  final bool isCurrentUser;
+  final bool finishedQuiz;
+  final DateTime? finishedAt;
+
+  PlayerResult({
+    required this.userId,
+    required this.username,
+    required this.score,
+    required this.correctAnswers,
+    required this.totalQuestions,
+    required this.rank,
+    required this.isCurrentUser,
+    required this.finishedQuiz,
+    this.finishedAt,
+  });
+
+  @override
+  String toString() {
+    return 'PlayerResult{username: $username, score: $score, rank: $rank, finished: $finishedQuiz}';
+  }
+}

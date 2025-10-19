f53f8da26a3e65e0abd1b6e7deac171cbb1b5664
Siamoo <walidsyam.f@gmail.com>
2025-10-19 03:10:51 +0300

    Update RoomJoiningBody

--- lib/features/quiz/room/presentation/refactors/room_joining_body.dart
+++ lib/features/quiz/room/presentation/refactors/room_joining_body.dart
@@ -1,23 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:znoona_game_app/core/common/animations/animate_do.dart';
 import 'package:znoona_game_app/core/common/widgets/custom_app_bar.dart';
-import 'package:znoona_game_app/core/common/widgets/custom_linear_button.dart';
 import 'package:znoona_game_app/core/common/widgets/custom_text_filed.dart';
-import 'package:znoona_game_app/core/common/widgets/text_app.dart';
-import 'package:znoona_game_app/core/helpers/znoona.colors.dart';
 import 'package:znoona_game_app/core/helpers/znoona_navigate.dart';
 import 'package:znoona_game_app/core/helpers/znoona_texts.dart';
 import 'package:znoona_game_app/core/language/lang_keys.dart';
 import 'package:znoona_game_app/core/style/images/app_images.dart';
 import 'package:znoona_game_app/features/quiz/room/presentation/cubit/room_cubit.dart';
 import 'package:znoona_game_app/features/quiz/room/presentation/screen/room_lobby_screen.dart';
+import 'package:znoona_game_app/features/quiz/room/presentation/widgets/joining/enter_code_or_scan.dart';
+import 'package:znoona_game_app/features/quiz/room/presentation/widgets/joining/join_room_button.dart';
+import 'package:znoona_game_app/features/quiz/room/presentation/widgets/joining/scan_qr_button.dart';
 
 class RoomJoiningBody extends StatefulWidget {
   const RoomJoiningBody({super.key});
-
   @override
   State<RoomJoiningBody> createState() => _RoomJoiningBodyState();
 }
@@ -25,16 +23,29 @@ class RoomJoiningBody extends StatefulWidget {
 class _RoomJoiningBodyState extends State<RoomJoiningBody> {
   final TextEditingController _codeController = TextEditingController();
   final _formKey = GlobalKey<FormState>();
+  final ScrollController _scrollController = ScrollController();
 
   @override
   void dispose() {
     _codeController.dispose();
+    _scrollController.dispose();
     super.dispose();
+  }
+
+  void _ensureTextFieldVisible() {
+    Future.delayed(const Duration(milliseconds: 300), () {
+      _scrollController.animateTo(
+        _scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+      );
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true, 
       body: BlocListener<RoomCubit, RoomState>(
         listener: (context, state) async {
           await state.whenOrNull(
@@ -55,7 +66,8 @@ class _RoomJoiningBodyState extends State<RoomJoiningBody> {
           );
         },
         child: SafeArea(
-          child: Padding(
+          child: SingleChildScrollView(
+            controller: _scrollController,
             padding: EdgeInsets.symmetric(
               horizontal: 24.w,
             ),
@@ -63,36 +75,23 @@ class _RoomJoiningBodyState extends State<RoomJoiningBody> {
               children: [
                 CustomAppBar(title: ZnoonaTexts.tr(context, LangKeys.joinRoom)),
                 SizedBox(height: 50.h),
-                SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      CustomFadeInDown(
-                        duration: 850,
-                        child: Image.asset(
-                          AppImages.join,
-                          height: 250.h,
-                          width: 250.w,
-                        ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CustomFadeInDown(
+                      duration: 850,
+                      child: Image.asset(
+                        AppImages.join,
+                        height: 250.h,
+                        width: 250.w,
                       ),
-                      const SizedBox(height: 24),
-                      CustomFadeInDown(
-                        duration: 700,
-                        child: TextApp(
-                          text: ZnoonaTexts.tr(
-                            context,
-                            LangKeys.enterCodeOrScan,
-                          ),
-                          textStyle: GoogleFonts.beiruti(
-                            fontSize: 24.sp,
-                            fontWeight: FontWeight.bold,
-                            color: ZnoonaColors.text(context),
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                    const SizedBox(height: 24),
+                    const CustomFadeInDown(
+                      duration: 700,
+                      child: EnterCodeOrScan(),
+                    ),
+                  ],
                 ),
                 SizedBox(height: 20.h),
                 Form(
@@ -110,15 +109,21 @@ class _RoomJoiningBodyState extends State<RoomJoiningBody> {
                           keyboardType: TextInputType.number,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
-                              //todo : localization , and handle when open keyboard
-                              return 'Please enter room code';
+                              return ZnoonaTexts.tr(
+                                context,
+                                LangKeys.pleaseEnterRoomCode,
+                              );
                             }
                             if (value.length != 6) {
-                              return 'Room code must be 6 characters';
+                              return ZnoonaTexts.tr(
+                                context,
+                                LangKeys.roomCodeMustBe6,
+                              );
                             }
                             return null;
                           },
                           suffixIcon: const Icon(Icons.key),
+                          onTap: _ensureTextFieldVisible, // Add this
                         ),
                       ),
                       SizedBox(height: 10.h),
@@ -132,77 +137,24 @@ class _RoomJoiningBodyState extends State<RoomJoiningBody> {
                             width: double.infinity,
                             child: CustomFadeInDown(
                               duration: 500,
-                              child: CustomLinearButton(
-                                height: 50.h,
-                                onPressed: isLoading
-                                    ? null
-                                    : () {
-                                        if (_formKey.currentState!.validate()) {
-                                          context.read<RoomCubit>().joinRoom(
-                                            code: _codeController.text.trim(),
-                                          );
-                                        }
-                                      },
-
-                                child: isLoading
-                                    ? const SizedBox(
-                                        height: 20,
-                                        width: 20,
-                                        child: CircularProgressIndicator(
-                                          strokeWidth: 2,
-                                          valueColor: AlwaysStoppedAnimation(
-                                            Colors.white,
-                                          ),
-                                        ),
-                                      )
-                                    : Text(
-                                        ZnoonaTexts.tr(
-                                          context,
-                                          LangKeys.joinRoom,
-                                        ),
-                                        style: const TextStyle(
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white,
-                                        ),
-                                      ),
+                              child: JoinRoomButton(
+                                isLoading: isLoading,
+                                formKey: _formKey,
+                                codeController: _codeController,
                               ),
                             ),
                           );
                         },
                       ),
-
                       SizedBox(height: 10.h),
-                      CustomFadeInDown(
+                      const CustomFadeInDown(
                         duration: 400,
-                        child: CustomLinearButton(
-                          height: 50.h,
-                          width: double.infinity,
-                          onPressed: () async {
-                            await _showQRScannerDialog(context);
-                          },
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(
-                                Icons.qr_code_scanner,
-                                color: ZnoonaColors.text(context),
-                              ),
-                              SizedBox(width: 10.w),
-                              Text(
-                                ZnoonaTexts.tr(
-                                  context,
-                                  LangKeys.scanQr,
-                                ),
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                  color: ZnoonaColors.text(context),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
+                        child: ScanQrButton(),
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).viewInsets.bottom > 0
+                            ? 100.h
+                            : 20.h,
                       ),
                     ],
                   ),
@@ -211,29 +163,6 @@ class _RoomJoiningBodyState extends State<RoomJoiningBody> {
             ),
           ),
         ),
-      ),
-    );
-  }
-
-  Future<void> _showQRScannerDialog(BuildContext context) async {
-    await showDialog<dynamic>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Scan QR Code'),
-        content: const Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Icon(Icons.qr_code_scanner, size: 60, color: Colors.grey),
-            SizedBox(height: 16),
-            Text('QR Scanner functionality coming soon!'),
-          ],
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: const Text('OK'),
-          ),
-        ],
       ),
     );
   }

2d7a5a1bbd32ad61bec8c5e46e812a7ca58af17f
Siamoo <walidsyam.f@gmail.com>
2025-10-22 05:49:13 +0300

    Update PlayerCircleImage to showplayer avatarUrl in Lobby

--- lib/features/quiz/room/presentation/widgets/lobby/player_circle_image.dart
+++ lib/features/quiz/room/presentation/widgets/lobby/player_circle_image.dart
@@ -4,24 +4,41 @@ import 'package:znoona_game_app/features/quiz/room/domain/entities/room_player.d
 
 class PlayerCircleImage extends StatelessWidget {
   const PlayerCircleImage({
-    required this.player, super.key,
+    required this.player,
+    super.key,
   });
 
   final RoomPlayer player;
 
   @override
   Widget build(BuildContext context) {
-    
     return Stack(
       children: [
         CircleAvatar(
           backgroundColor: player.isHost
               ? ZnoonaColors.main(context)
               : Colors.grey.shade300,
-          child: Icon(
-            player.isHost ? Icons.star : Icons.person,
-            color: player.isHost ? Colors.white : Colors.grey.shade600,
-          ),
+          child: player.avatarUrl != null && player.avatarUrl!.isNotEmpty
+              ? ClipOval(
+                  child: Image.network(
+                    player.avatarUrl!,
+                    width: 40,
+                    height: 40,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Icon(
+                        Icons.person,
+                        color: player.isHost
+                            ? Colors.white
+                            : Colors.grey.shade600,
+                      );
+                    },
+                  ),
+                )
+              : Icon(
+                  player.isHost ? Icons.star : Icons.person,
+                  color: player.isHost ? Colors.white : Colors.grey.shade600,
+                ),
         ),
         if (player.isHost)
           Positioned(

f76410aa23e89c1d2721990b7749d474be18789a
Siamoo <walidsyam.f@gmail.com>
2025-10-29 00:13:12 +0300

    Add getMainCategories in CategoriesRepositoryImpl

--- lib/features/quiz/categories/data/repositories/categories_repository_impl.dart
+++ lib/features/quiz/categories/data/repositories/categories_repository_impl.dart
@@ -4,18 +4,29 @@ import 'package:znoona_game_app/features/quiz/categories/data/models/category_mo
 import 'package:znoona_game_app/features/quiz/categories/domain/entities/category.dart';
 import 'package:znoona_game_app/features/quiz/categories/domain/repositories/categories_repository.dart';
 
-
 class CategoriesRepositoryImpl implements CategoriesRepository {
-  CategoriesRepositoryImpl(this.remote);
-  final CategoriesRemoteDataSource remote;
+  CategoriesRepositoryImpl(this.remoteDataSource);
+  final CategoriesRemoteDataSource remoteDataSource;
 
   @override
   Future<Either<String, List<Category>>> getCategories() async {
     try {
-      final result = await remote.getCategories();
-      return Right(result.map((m) => m.toEntity()).toList());
-    } on Exception catch (e) {
+      final  result = await remoteDataSource.getCategories();
+      return Right(result.map((model) => model.toEntity()).toList());
+    }on Exception catch (e) {
       return Left(e.toString());
     }
   }
+
+  @override
+  Future<Either<String, List<Category>>> getMainCategories() async {
+    try {
+      final  result = await remoteDataSource.getMainCategories();
+      return Right(result.map((model) => model.toEntity()).toList());
+    }on Exception catch (e) {
+      return Left(e.toString());
+    }
+  }
+
+
 }

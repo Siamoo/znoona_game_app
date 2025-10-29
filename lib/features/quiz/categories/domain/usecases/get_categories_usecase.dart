7b2885f7e15df1dccc8a4694b921f531164f8b3b
Siamoo <walidsyam.f@gmail.com>
2025-10-30 00:15:58 +0300

    Update GetCategoriesUseCase

--- lib/features/quiz/categories/domain/usecases/get_categories_usecase.dart
+++ lib/features/quiz/categories/domain/usecases/get_categories_usecase.dart
@@ -1,4 +1,3 @@
-
 import 'package:dartz/dartz.dart';
 import 'package:znoona_game_app/features/quiz/categories/domain/entities/category.dart';
 import 'package:znoona_game_app/features/quiz/categories/domain/repositories/categories_repository.dart';

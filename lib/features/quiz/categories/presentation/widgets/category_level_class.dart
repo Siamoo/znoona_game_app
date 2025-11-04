e936cec9f4d204dc2672910b936dda2c30abc3e4
Siamoo <walidsyam.f@gmail.com>
2025-11-04 06:03:21 +0200

    Remove LevelType enum

--- lib/features/quiz/categories/presentation/widgets/category_level_class.dart
+++ lib/features/quiz/categories/presentation/widgets/category_level_class.dart
@@ -1,20 +1,9 @@
 
 class CategoryLevel {
   CategoryLevel({
-    required this.type,
     required this.title,
     this.parentId,
   });
-  final LevelType type;
   final String title;
   final String? parentId;
-}
-
-enum LevelType {
-  main,
-  faculty,
-  year,
-  subject,
-  unit,
-  lesson,
 }

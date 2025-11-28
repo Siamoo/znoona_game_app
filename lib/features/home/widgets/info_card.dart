8bf619d1f90688222fb0436dc7af844225f9727c
Siamoo <walidsyam.f@gmail.com>
2025-11-28 03:29:34 +0200

    Create InfoCard

---
+++ lib/features/home/widgets/info_card.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:znoona_game_app/core/common/widgets/custom_linear_button.dart';
+import 'package:znoona_game_app/core/helpers/znoona.colors.dart';
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({
+    required this.title,
+    required this.value,
+    required this.icon,
+    required this.color,
+    super.key,
+  });
+
+  final String title;
+  final String value;
+  final IconData icon;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomLinearButton(
+      borderRadiusNum: 12,
+      colors: [
+        color.withOpacity(0.1),
+        color.withOpacity(0.2),
+        color.withOpacity(0.05),
+        color.withOpacity(0.1),
+      ],
+      height: 110.h,
+      width: double.infinity,
+      onPressed: () {},
+      child: Padding(
+        padding: EdgeInsets.all(8.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icon
+            Icon(
+              icon,
+              color: color,
+              size: 20.sp,
+            ),
+            SizedBox(height: 6.h),
+            // Value
+            Text(
+              value,
+              style: GoogleFonts.beiruti(
+                fontSize: 12.sp,
+                fontWeight: FontWeight.w600,
+                color: ZnoonaColors.text(context),
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            SizedBox(height: 4.h),
+            // Title
+            Text(
+              title,
+              style: GoogleFonts.beiruti(
+                fontSize: 10.sp,
+                fontWeight: FontWeight.w500,
+                color: ZnoonaColors.text(context)?.withOpacity(0.7),
+              ),
+              textAlign: TextAlign.center,
+              maxLines: 1,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

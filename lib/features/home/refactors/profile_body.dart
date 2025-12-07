16f582fa3f37dd5a80bbeeda21b285059f610a1f
Siamoo <walidsyam.f@gmail.com>
2025-12-07 14:46:34 +0200

    Remove edit icon from ProfileBody

--- lib/features/home/refactors/profile_body.dart
+++ lib/features/home/refactors/profile_body.dart
@@ -11,7 +11,6 @@ import 'package:znoona_game_app/features/home/widgets/info_card.dart';
 import 'package:znoona_game_app/features/home/widgets/settings_buttons.dart';
 import 'package:znoona_game_app/features/home/widgets/stat_card.dart';
 import 'package:znoona_game_app/features/user/auth/domain/entities/profile.dart';
-import 'package:znoona_game_app/features/user/auth/presentation/refactors/username_edit_body.dart';
 
 class ProfileBody extends StatelessWidget {
   const ProfileBody({required this.profile, super.key});
@@ -260,32 +259,6 @@ class ProfileBody extends StatelessWidget {
                             fontSize: 16.sp,
                             fontWeight: FontWeight.w600,
                             color: ZnoonaColors.bluePinkDark(context),
-                          ),
-                        ),
-                        SizedBox(width: 8.w),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) =>
-                                    const UsernameEditBody(),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            padding: EdgeInsets.all(4.w),
-                            decoration: BoxDecoration(
-                              color: ZnoonaColors.main(
-                                context,
-                              )!.withOpacity(0.1),
-                              shape: BoxShape.circle,
-                            ),
-                            child: Icon(
-                              Icons.edit,
-                              size: 14.sp,
-                              color: ZnoonaColors.main(context),
-                            ),
                           ),
                         ),
                       ],

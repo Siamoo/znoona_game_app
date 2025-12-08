2c401c55005da8325ed8749d10d3c52fb9b97a80
Siamoo <walidsyam.f@gmail.com>
2025-12-09 00:16:31 +0200

    Update AppImages

--- lib/core/style/images/app_images.dart
+++ lib/core/style/images/app_images.dart
@@ -6,17 +6,10 @@ class AppImages {
   // user
   static const userAvatar = 'assets/images/user/user.png';
   static const level = 'assets/images/user/level.png';
+  static const cup = 'assets/images/user/cup.png';
   static const quizGame = 'assets/images/user/quiz_game.png';
   static const puzzle = 'assets/images/user/Puzzle.png';
   static const discover = 'assets/images/user/discover.png';
-  static const food = 'assets/images/user/food.png';
-  static const football = 'assets/images/user/football.png';
-  static const geography = 'assets/images/user/geography.png';
-  static const history = 'assets/images/user/history.png';
-  static const movies = 'assets/images/user/movies.png';
-  static const random = 'assets/images/user/rondem.png';
-  static const science = 'assets/images/user/science.png';
-  static const technology = 'assets/images/user/technology.png';
   static const wonCup = 'assets/images/user/won_cup.png';
   static const goodResult = 'assets/images/user/good_result.png';
   static const notGood = 'assets/images/user/not_good.png';
@@ -26,6 +19,7 @@ class AppImages {
   static const create = 'assets/images/user/Create.png';
   static const qrScanner = 'assets/images/user/qr_scanner.png';
   static const permission = 'assets/images/user/permission.png';
+  static const science = 'assets/images/user/science.png';
 
   //Svg
   static const mode = 'assets/svg/dark_mode.svg';

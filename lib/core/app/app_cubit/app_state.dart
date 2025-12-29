c11b88e4d0ace660c8ccbd3bd4b6db360524f1ad
Siamoo <walidsyam.f@gmail.com>
2025-12-29 16:23:16 +0200

    Update app state

--- lib/core/app/app_cubit/app_state.dart
+++ lib/core/app/app_cubit/app_state.dart
@@ -2,9 +2,27 @@ part of 'app_cubit.dart';
 
 @freezed
 class AppState with _$AppState {
-  const factory AppState.initial() = _Initial;
-  const factory AppState.themeChangMode({required bool isDarkMode}) =
-      ThemeChangModeState;
-  const factory AppState.languageChange({required Locale locale}) =
-      LanguageChangState;
+  const factory AppState({
+    // Theme
+    required bool isDarkMode,
+    
+    // Language
+    required Locale locale,
+    
+    // Audio Settings
+    required bool isSoundEnabled,
+    required bool isBackgroundMusicEnabled,
+    required double soundVolume,
+    required double musicVolume,
+  }) = _AppState;
+
+  // Factory for initial state
+  factory AppState.initial() => const AppState(
+    isDarkMode: false,
+    locale: Locale('en'),
+    isSoundEnabled: true,
+    isBackgroundMusicEnabled: true,
+    soundVolume: 1.0,
+    musicVolume: 0.5,
+  );
 }

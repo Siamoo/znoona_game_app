e43a046cfd5d190822f4eacd6ec9ff956a7ea1a3
Siamoo <walidsyam.f@gmail.com>
2026-01-01 16:33:41 +0200

    Update ZnoonaGameApp

--- lib/znoona_game_app.dart
+++ lib/znoona_game_app.dart
@@ -6,17 +6,31 @@ import 'package:medaan_almaarifa/core/app/app_cubit/app_cubit.dart';
 import 'package:medaan_almaarifa/core/app/connectivity_controller.dart';
 import 'package:medaan_almaarifa/core/common/screens/no_network_screen.dart';
 import 'package:medaan_almaarifa/core/di/injcetion_container.dart';
+import 'package:medaan_almaarifa/core/helpers/audio_service.dart';
 import 'package:medaan_almaarifa/core/language/app_localizations_setup.dart';
-import 'package:medaan_almaarifa/core/service/shared_pref/pref_keys.dart';
-import 'package:medaan_almaarifa/core/service/shared_pref/shared_pref.dart';
 import 'package:medaan_almaarifa/core/style/theme/app_theme.dart';
 import 'package:medaan_almaarifa/features/home/screens/home_screen.dart';
 import 'package:medaan_almaarifa/features/quiz/categories/presentation/cubit/categories_cubit.dart';
 import 'package:medaan_almaarifa/features/quiz/room/presentation/cubit/room_cubit.dart';
 import 'package:medaan_almaarifa/features/user/auth/presentation/cubit/auth_cubit.dart';
 import 'package:medaan_almaarifa/features/user/auth/presentation/screens/login_screen.dart';
-class ZnoonaGameApp extends StatelessWidget {
+
+class ZnoonaGameApp extends StatefulWidget {
   const ZnoonaGameApp({super.key});
+
+  @override
+  State<ZnoonaGameApp> createState() => _ZnoonaGameAppState();
+}
+
+class _ZnoonaGameAppState extends State<ZnoonaGameApp> {
+  @override
+  void initState() {
+    super.initState();
+    // Initialize connectivity after widget is mounted
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      ConnectivityController.instance.init();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +42,7 @@ class ZnoonaGameApp extends StatelessWidget {
         return MultiBlocProvider(
           providers: [
             BlocProvider(
-              create: (context) => sl<AppCubit>()
-                ..changeAppThemeMode(
-                  sheredMode: SharedPref().getBoolean(PrefKeys.themeMode),
-                )
-                ..getSavedLanguage(),
+              create: (context) => sl<AppCubit>(),
             ),
             BlocProvider(create: (_) => sl<AuthCubit>()..getCurrentUser()),
             BlocProvider(create: (_) => sl<RoomCubit>()),
@@ -45,20 +55,29 @@ class ZnoonaGameApp extends StatelessWidget {
             builder: (_, __) => BlocBuilder<AppCubit, AppState>(
               buildWhen: (previous, current) => previous != current,
               builder: (context, state) {
-                final cubit = context.read<AppCubit>();
-
+                // **FIXED: Only update AudioService if it's initialized**
+                // This prevents calling onAppStateChanged before initialization
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  try {
+                    final audioService = sl<AudioService>();
+                    // Check if audio service is ready before updating
+                    audioService.onAppStateChanged(state);
+                  } catch (e) {
+                    print('⚠️ Error updating AudioService with state: $e');
+                  }
+                });
+                
                 return MaterialApp(
                   debugShowCheckedModeBanner: false,
                   title: 'Znoona Game App',
-                  theme: cubit.isDarkMode ? darkTheme() : lightTheme(),
-                  locale: Locale(cubit.currentLangcode),
+                  theme: state.isDarkMode ? darkTheme() : lightTheme(),
+                  locale: state.locale,
                   supportedLocales: AppLocalizationsSetup.supportedLocales,
                   localeResolutionCallback:
                       AppLocalizationsSetup.localeResolutionCallback,
                   localizationsDelegates:
                       AppLocalizationsSetup.localizationsDelegates,
 
-              
                   builder: (context, child) {
                     final mediaQuery = MediaQuery.of(context);
                     // Option 1: Fully disable system font scaling
@@ -70,17 +89,10 @@ class ZnoonaGameApp extends StatelessWidget {
                         onTap: () =>
                             FocusManager.instance.primaryFocus?.unfocus(),
                         child: Scaffold(
-                          body: Builder(
-                            builder: (context) {
-                              ConnectivityController.instance.init();
-                              return child!;
-                            },
-                          ),
+                          body: child!,
                         ),
                       ),
                     );
-
-
                   },
 
                   home: !value

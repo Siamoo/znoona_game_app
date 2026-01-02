f48c18d5ef06c211008f0c8bf6607423303697c7
Siamoo <walidsyam.f@gmail.com>
2026-01-02 16:36:59 +0200

    update SettingsButtons

--- lib/features/home/widgets/settings_buttons.dart
+++ lib/features/home/widgets/settings_buttons.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medaan_almaarifa/features/home/refactors/sound_setting_body.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:medaan_almaarifa/core/app/app_cubit/app_cubit.dart';
 import 'package:medaan_almaarifa/core/helpers/znoona_navigate.dart';
@@ -10,6 +11,8 @@ import 'package:medaan_almaarifa/features/home/widgets/custom_setting_button.dar
 import 'package:medaan_almaarifa/features/user/auth/presentation/refactors/profile_edit_body.dart';
 import 'package:medaan_almaarifa/features/user/auth/presentation/screens/login_screen.dart';
 
+// NEW: Add Sound Settings Screen
+
 class SettingsButtons extends StatelessWidget {
   const SettingsButtons({
     super.key,
@@ -17,13 +20,13 @@ class SettingsButtons extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final cubit = context.read<AppCubit>();
     return Column(
       children: [
         CustomSettingButton(
           title: LangKeys.changeLanguage,
           onTap: () {
-            if (cubit.currentLangcode == 'en') {
+            final cubit = context.read<AppCubit>();
+            if (cubit.state.locale.languageCode == 'en') {
               cubit.toArabic();
             } else {
               cubit.toEnglish();
@@ -34,8 +37,20 @@ class SettingsButtons extends StatelessWidget {
         SizedBox(height: 20.h),
         CustomSettingButton(
           title: LangKeys.changeTheme,
-          onTap: cubit.changeAppThemeMode,
+          onTap: () => context.read<AppCubit>().toggleTheme(),
           imageUrl: AppImages.mode,
+        ),
+        SizedBox(height: 20.h),
+        // NEW: Sound Settings Button
+        CustomSettingButton(
+          title: LangKeys.soundSettings,
+          onTap: () {
+            ZnoonaNavigate.pushTo(
+              context,
+              const SoundSettingsScreen(),
+            );
+          },
+          imageUrl: AppImages.volume, // You'll need to add this image
         ),
         SizedBox(height: 20.h),
         CustomSettingButton(

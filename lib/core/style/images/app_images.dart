12835a0a4a5d8b320e0fd566ea6ccadd0c8bbbc5
Siamoo <walidsyam.f@gmail.com>
2026-01-25 03:06:32 +0200

    Update AppImages

--- lib/core/style/images/app_images.dart
+++ lib/core/style/images/app_images.dart
@@ -2,6 +2,10 @@ class AppImages {
   //Images
   static const noNetwork = 'assets/images/core/no_network.png';
   static const error = 'assets/images/core/error.png';
+  static const callUs = 'assets/images/core/call_us.png';
+  static const facebook = 'assets/images/core/facebook.png';
+  static const email = 'assets/images/core/email.png';
+  static const whatsapp = 'assets/images/core/whatsapp.png';
 
   // user
   static const userAvatar = 'assets/images/user/user.png';
@@ -20,6 +24,12 @@ class AppImages {
   static const qrScanner = 'assets/images/user/qr_scanner.png';
   static const permission = 'assets/images/user/permission.png';
   static const science = 'assets/images/user/science.png';
+  static const bike = 'assets/images/design/bike.png';
+  static const greenMWake = 'assets/images/design/g_m_wake.png';
+  static const greenMWake2 = 'assets/images/design/g_m_walk_2.png';
+  static const redWRightSide = 'assets/images/design/r_w_right_side.png';
+  static const yelloWHand = 'assets/images/design/y_w_hand.png';
+  static const yelloWThink = 'assets/images/design/y_w_think.png';
 
   //Svg
   static const mode = 'assets/svg/dark_mode.svg';

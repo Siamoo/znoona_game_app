a92504bca20a4ae6c391268907443aa4a81c1e04
Siamoo <walidsyam.f@gmail.com>
2026-01-27 03:11:52 +0200

    Update QuizHomeBody

--- lib/features/home/refactors/quiz_home_body.dart
+++ lib/features/home/refactors/quiz_home_body.dart
@@ -6,7 +6,6 @@ import 'package:medaan_almaarifa/core/language/lang_keys.dart';
 import 'package:medaan_almaarifa/core/style/images/app_images.dart';
 import 'package:medaan_almaarifa/features/home/widgets/custom_game_button.dart';
 import 'package:medaan_almaarifa/features/home/widgets/custom_section_title.dart';
-import 'package:medaan_almaarifa/features/home/widgets/discover_button.dart';
 import 'package:medaan_almaarifa/features/quiz/categories/presentation/refactors/category_hierarchy_body.dart';
 import 'package:medaan_almaarifa/features/quiz/room/presentation/screen/room_screen.dart';
 
@@ -17,73 +16,93 @@ class QuizHomeBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 10.w,
-              vertical: 10.h,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomAppBar(title: LangKeys.quizGame),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 10.w,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 30.h),
-                      const CustomSectionTitle(
-                        langkey: LangKeys.chooseMode,
-                      ),
-                      SizedBox(height: 10.h),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          GameButton(
-                            image: AppImages.solo,
-                            langkey: LangKeys.solo,
-                            onPressed: () => ZnoonaNavigate.pushTo(
-                              context,
-                              const CategoryHierarchyBody(
-                                isRoom: false,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.w,
+                vertical: 10.h,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CustomAppBar(title: LangKeys.quizGame),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10.w,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 30.h),
+                        const CustomSectionTitle(
+                          langkey: LangKeys.chooseMode,
+                        ),
+                        SizedBox(height: 10.h),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            GameButton(
+                              image: AppImages.solo,
+                              langkey: LangKeys.solo,
+                              onPressed: () => ZnoonaNavigate.pushTo(
+                                context,
+                                const CategoryHierarchyBody(
+                                  isRoom: false,
+                                ),
                               ),
                             ),
-                          ),
-                          GameButton(
-                            image: AppImages.multi,
-                            langkey: LangKeys.room,
-                            onPressed: () {
-                              ZnoonaNavigate.pushTo(
-                                context,
-                                const RoomScreen(),
-                              );
-                            },
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 30.h),
-                      const CustomSectionTitle(
-                        langkey: LangKeys.letsDiscover,
-                      ),
-                      SizedBox(height: 10.h),
-                      const DiscoverButton(),
-                      SizedBox(height: 30.h),
-                      const CustomSectionTitle(
-                        langkey: LangKeys.universityStudents,
-                      ),
-                      SizedBox(height: 10.h),
-                      const DiscoverButton(),
-                    ],
+                            GameButton(
+                              image: AppImages.multi,
+                              langkey: LangKeys.room,
+                              onPressed: () {
+                                ZnoonaNavigate.pushTo(
+                                  context,
+                                  const RoomScreen(),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
 
-                SizedBox(height: 20.h),
-              ],
+                  SizedBox(height: 20.h),
+                ],
+              ),
             ),
-          ),
+            SizedBox(
+              height: 300.h,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 0,
+                    bottom: 0,
+                    child: Image.asset(
+                      AppImages.yelloWHand,
+                      height: 100.h,
+                    ),
+                  ),
+
+                  // Wake in center
+                  Positioned(
+                    left: 45.w,
+                    right: 0,
+                    bottom: 0,
+                    child: Center(
+                      child: Image.asset(
+                        AppImages.yelloWThink,
+                        height: 300.h,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );

70b2bdfcc2dcdfd5f341d99f0f677cdba244d457
Siamoo <walidsyam.f@gmail.com>
2026-01-31 01:57:53 +0200

    Update RoomBody

--- lib/features/quiz/room/presentation/refactors/room_body.dart
+++ lib/features/quiz/room/presentation/refactors/room_body.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -18,60 +19,121 @@ class RoomBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 10.w,
-              vertical: 10.h,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CustomAppBar(title: LangKeys.quizGame),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 10.w,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: 30.h),
-                      const CustomSectionTitle(
-                        langkey: LangKeys.multiplayer,
-                      ),
-                      SizedBox(height: 10.h),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          GameButton(
-                            image: AppImages.create,
-                            langkey: LangKeys.createRoom,
-                            onPressed: () {
-                              ZnoonaNavigate.pushTo(
-                                context,
-                                const CategoryHierarchyBody(isRoom: true),
-                              );
-                            },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 10.w,
+                vertical: 10.h,
+              ),
+              child: Column(
+                children: [
+                  const CustomAppBar(title: LangKeys.quizGame),
+                  SizedBox(height: 30.h),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 10.w,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children: [
+                        FadeInLeft(
+                          child: const CustomSectionTitle(
+                            langkey: LangKeys.multiplayer,
                           ),
-                          GameButton(
-                            image: AppImages.join,
-                            langkey: LangKeys.joinRoom,
-                            onPressed: () async {
-                              context.read<RoomCubit>().leaveFromAllRooms();
-                              await ZnoonaNavigate.pushTo(
-                                context,
-                                const RoomJoiningScreen(),
-                              );
-                            },
+                        ),
+                        SizedBox(height: 10.h),
+                        FadeInDown(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              FadeInLeft(
+                                child: GameButton(
+                                  image: AppImages.create,
+                                  langkey: LangKeys.createRoom,
+                                  onPressed: () {
+                                    ZnoonaNavigate.pushTo(
+                                      context,
+                                      const CategoryHierarchyBody(isRoom: true),
+                                    );
+                                  },
+                                ),
+                              ),
+                              FadeInRight(
+                                child: GameButton(
+                                  image: AppImages.join,
+                                  langkey: LangKeys.joinRoom,
+                                  onPressed: () async {
+                                    context
+                                        .read<RoomCubit>()
+                                        .leaveFromAllRooms();
+                                    await ZnoonaNavigate.pushTo(
+                                      context,
+                                      const RoomJoiningScreen(),
+                                    );
+                                  },
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+            SizedBox(
+              height: 400.h,
+              width: double.infinity,
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 40.w,
+                    bottom: 200.h,
+                    child: FadeInLeft(
+                      duration: const Duration(milliseconds: 4000),
+                      child: Image.asset(
+                        AppImages.airplane,
+                        height: 70.h,
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                    left: 0,
+                    right: 80.w,
+                    bottom: 0,
+                    child: FadeInLeft(
+                      duration: const Duration(milliseconds: 1400),
+                      child: Image.asset(
+                        AppImages.blackMNo,
+                        height: 130.h,
+                      ),
+                    ),
+                  ),
+
+                  Positioned(
+                    left: 200.w,
+                    right: 0,
+                    bottom: 0,
+                    child: FadeInRight(
+                      duration: const Duration(milliseconds: 1400),
+                      child: Center(
+                        child: Image.asset(
+                          AppImages.redWTraveler,
+                          height: 110.h,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );

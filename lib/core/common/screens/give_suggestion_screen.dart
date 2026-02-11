ef5e22a6627781ffe63c7847febd54fd9eb83dc3
Siamoo <walidsyam.f@gmail.com>
2026-02-11 09:18:46 +0200

    Add animation to GiveSuggestionScreen

--- lib/core/common/screens/give_suggestion_screen.dart
+++ lib/core/common/screens/give_suggestion_screen.dart
@@ -57,136 +57,208 @@ class GiveSuggestionScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.w),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                FadeInDown(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(width: 40.w),
-                      Center(
-                        child: TextApp(
-                          text: ZnoonaTexts.tr(context, LangKeys.suggestions),
-                          textStyle: GoogleFonts.beiruti(
-                            fontSize: 26.h,
-                            fontWeight: FontWeight.bold,
-                            color: ZnoonaColors.text(context),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.close,
-                          size: 30.h,
-                          color: ZnoonaColors.text(context),
-                        ),
-                        onPressed: () => Navigator.of(context).pop(),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20.h),
-
-                // Header Image
-                FadeInDown(
-                  delay: const Duration(milliseconds: 200),
-                  child: Center(
-                    child: Image.asset(
-                      AppImages.puzzle,
-                      height: 170.h,
-                      width: 170.w,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Icon(
-                          Icons.support_agent,
-                          size: 80.h,
-                          color: ZnoonaColors.bluePinkDark(context),
-                        );
-                      },
-                    ),
-                  ),
-                ),
-
-                SizedBox(height: 16.h),
-
-                // Title
-                FadeInDown(
-                  delay: const Duration(milliseconds: 300),
-                  child: TextApp(
-                    text: ZnoonaTexts.tr(context, LangKeys.howCanWeHelp),
-                    textStyle: GoogleFonts.beiruti(
-                      fontSize: 24.h,
-                      fontWeight: FontWeight.bold,
-                      color: ZnoonaColors.text(context),
-                    ),
-                  ),
-                ),
-
-                SizedBox(height: 8.h),
-
-                // Subtitle
-                FadeInDown(
-                  delay: const Duration(milliseconds: 400),
-                  child: TextApp(
-                    text: ZnoonaTexts.tr(context, LangKeys.newAppSuggestions),
-                    textStyle: GoogleFonts.beiruti(
-                      fontSize: 18.h,
-                      color: Colors.grey.shade600,
-                    ),
-                    maxLines: 4,
-                    textAlign: TextAlign.center,
-                  ),
-                ),
-
-                SizedBox(height: 30.h),
-
-                // WhatsApp Button
-                FadeInDown(
-                  delay: const Duration(milliseconds: 500),
-                  child: CustomLinearButton(
-                    borderRadiusNum: 20,
-                    colors: [
-                      Colors.green.shade600,
-                      Colors.green.shade900,
-                    ],
-                    height: 70.h,
-                    width: double.infinity,
-                    onPressed: () => _openWhatsApp(context),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16.w),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            AppImages.whatsapp,
-                            height: 50.h,
-                            width: 50.w,
-                            errorBuilder: (context, error, stackTrace) {
-                              return Icon(
-                                Icons.message,
-                                color: Colors.white,
-                                size: 30.h,
-                              );
-                            },
-                          ),
-                          SizedBox(width: 10.w),
-                          TextApp(
-                            text: ZnoonaTexts.tr(context, LangKeys.whatsapp),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  FadeInDown(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SizedBox(width: 40.w),
+                        Center(
+                          child: TextApp(
+                            text: ZnoonaTexts.tr(context, LangKeys.suggestions),
                             textStyle: GoogleFonts.beiruti(
-                              fontSize: 22.h,
-                              fontWeight: FontWeight.w800,
-                              color: Colors.white,
+                              fontSize: 26.h,
+                              fontWeight: FontWeight.bold,
+                              color: ZnoonaColors.text(context),
                             ),
                           ),
-                        ],
+                        ),
+                        IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            size: 30.h,
+                            color: ZnoonaColors.text(context),
+                          ),
+                          onPressed: () => Navigator.of(context).pop(),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.h),
+
+                  // Header Image
+                  FadeInDown(
+                    delay: const Duration(milliseconds: 200),
+                    child: Center(
+                      child: Image.asset(
+                        AppImages.puzzle,
+                        height: 170.h,
+                        width: 170.w,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Icon(
+                            Icons.support_agent,
+                            size: 80.h,
+                            color: ZnoonaColors.bluePinkDark(context),
+                          );
+                        },
                       ),
                     ),
                   ),
-                ),
 
-                SizedBox(height: 10.h),
-              ],
-            ),
+                  SizedBox(height: 16.h),
+
+                  // Title
+                  FadeInDown(
+                    delay: const Duration(milliseconds: 300),
+                    child: TextApp(
+                      text: ZnoonaTexts.tr(context, LangKeys.howCanWeHelp),
+                      textStyle: GoogleFonts.beiruti(
+                        fontSize: 24.h,
+                        fontWeight: FontWeight.bold,
+                        color: ZnoonaColors.text(context),
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: 8.h),
+
+                  // Subtitle
+                  FadeInDown(
+                    delay: const Duration(milliseconds: 400),
+                    child: TextApp(
+                      text: ZnoonaTexts.tr(context, LangKeys.newAppSuggestions),
+                      textStyle: GoogleFonts.beiruti(
+                        fontSize: 18.h,
+                        color: Colors.grey.shade600,
+                      ),
+                      maxLines: 4,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+
+                  SizedBox(height: 30.h),
+
+                  // WhatsApp Button
+                  FadeInDown(
+                    delay: const Duration(milliseconds: 500),
+                    child: CustomLinearButton(
+                      borderRadiusNum: 20,
+                      colors: [
+                        Colors.green.shade600,
+                        Colors.green.shade900,
+                      ],
+                      height: 70.h,
+                      width: double.infinity,
+                      onPressed: () => _openWhatsApp(context),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16.w),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              AppImages.whatsapp,
+                              height: 50.h,
+                              width: 50.w,
+                              errorBuilder: (context, error, stackTrace) {
+                                return Icon(
+                                  Icons.message,
+                                  color: Colors.white,
+                                  size: 30.h,
+                                );
+                              },
+                            ),
+                            SizedBox(width: 10.w),
+                            TextApp(
+                              text: ZnoonaTexts.tr(context, LangKeys.whatsapp),
+                              textStyle: GoogleFonts.beiruti(
+                                fontSize: 22.h,
+                                fontWeight: FontWeight.w800,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 250.h,
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      right: 100.w,
+                      bottom: 200.h,
+                      child: FadeInDown(
+                        duration: const Duration(seconds: 6),
+                        from: 50.h,
+                        curve: Curves.elasticOut,
+                        child: FadeInRight(
+                          duration: const Duration(seconds: 6),
+                          // curve: Curves.elasticOut,
+                          from: 200.w,
+                          child: Image.asset(
+                            AppImages.bird,
+                            height: 30.h,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      left: 10.w,
+                      bottom: 180.h,
+                      child: FadeInLeft(
+                        duration: const Duration(seconds: 10),
+                        from: 50.h,
+                        curve: Curves.elasticOut,
+                        child: FadeInDown(
+                          duration: const Duration(seconds: 10),
+                          from: 60.w,
+                          child: Image.asset(
+                            AppImages.birds,
+                            height: 30.h,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      right: 10.w,
+                      bottom: 0,
+                      child: FadeInRight(
+                        duration: const Duration(seconds: 2),
+                        from: 120,
+                        child: Image.asset(
+                          AppImages.blueMSkates,
+                          height: 90.h,
+                        ),
+                      ),
+                    ),
+
+                    Positioned(
+                      right: 200.w,
+                      bottom: 0,
+                      child: FadeInLeft(
+                        duration: const Duration(milliseconds: 1400),
+                        child: Center(
+                          child: Image.asset(
+                            AppImages.manBirds,
+                            height: 110.h,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),

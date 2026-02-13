3b56b65b4da19da3dffb862ec1d6b34a515e0905
Siamoo <walidsyam.f@gmail.com>
2026-02-13 15:11:50 +0200

    Create AnimationDurations & AnimationDelays & AnimationCurves

---
+++ lib/core/common/animations/animation_constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnimationDurations {
+  static const Duration fastest = Duration(milliseconds: 150);
+  static const Duration fast = Duration(milliseconds: 300);
+  static const Duration normal = Duration(milliseconds: 500);
+  static const Duration medium = Duration(milliseconds: 800);
+  static const Duration slow = Duration(milliseconds: 1200);
+  static const Duration slower = Duration(milliseconds: 1500);
+  static const Duration slowest = Duration(milliseconds: 2000);
+}
+
+class AnimationDelays {
+  static const Duration none = Duration.zero;
+  static const Duration short = Duration(milliseconds: 100);
+  static const Duration medium = Duration(milliseconds: 300);
+  static const Duration long = Duration(milliseconds: 500);
+}
+
+class AnimationCurves {
+  static const Curve defaultCurve = Curves.easeInOut;
+  static const Curve bounceOut = Curves.bounceOut;
+  static const Curve elasticOut = Curves.elasticOut;
+  static const Curve decelerate = Curves.decelerate;
+}
